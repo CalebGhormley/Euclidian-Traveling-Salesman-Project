6e160d6c27fde130337c203822e3e234b773da5a
Caleb Ghormley <calebghormley@yahoo.com>
2018-11-16 04:12:51 -0700

    Almost done with genTours

--- Project3.rkt
+++ Project3.rkt
@@ -1,30 +1,40 @@
 #lang racket
-(define (trevlist list acc)
-  (if (null? list)
-      acc
-      (trevlist (cdr list) (cons (car list) acc))
-                )
-      )
+(define (genTours n)
+  (permutate (trevlist (makeList n) '()) 0)
+  )
 
 (define (makeList n)
   (if (= n 0)
       null
-      (cons n (makeList (- n 1)))
-  )
-)
+      (cons n (makeList (- n 1)))))
 
-(define (permutate list)
-  (if (null? (cdr list))
-      null
-      (cons (cons '1 list) (cons (car (cdr list)) (cons '1 (cons (car list) (cdr (cdr list))))))
-      ) 
-)
-
-(define (remove list acc)
+(define (trevlist list acc)
   (if (null? list)
-      null
-      (cons (cdr list) (cons (car list) acc))))
+      acc
+      (trevlist (cdr list) (cons (car list) acc))))
 
-(define (genTours n)
-  (permutate (cdr (trevlist (makeList n) '())))
-  )
+(define (permutate list i)
+  (if (< i (factorial (length (cdr list))))
+      (cons (cons (car list) (permutation (cdr list) i 0)) (permutate list (+ i 1)))
+      list)
+)
+
+(define (permutation list n a)
+  (if (> a (length list))
+      (= a 1)
+      (= a a)
+      )
+  (if (> n 0)
+      (permutation (swapAt list a) (- n 1) (+ a 1))
+      list))
+
+(define (swapAt list a)
+  (if (> a 1)
+      (cons (car list) (swapAt (cdr list) (- a 1)))
+      (cons (car (cdr list)) (cons (car list) (cdr (cdr list))))))
+
+
+(define (factorial n)
+  (if (> n 1)
+      (* n (factorial (- n 1)))
+      1))
